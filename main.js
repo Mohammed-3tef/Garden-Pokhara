d9b2ee95c1c6db1468f54006c343e79fb53f5bc0
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-08-09 12:31:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let swiper = new Swiper(".mySwiper", {
-    navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
-   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
- });
+  navigation: {
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+ },
+ pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+ },
+});
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
